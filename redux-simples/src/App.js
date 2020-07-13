543e2b0e3fc7aa836763f99f1f7ecdbbc1bdd143
viannaandreBR <vianna_andre@hotmail.com@users.noreply.github.com>
2020-07-13 02:50:22 -0300

    Commit Component Card Color

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import Card from './components/Card'
 function App() {
@@ -7,20 +7,20 @@ function App() {
     <div className="App">
       
       <h1>Demo React-Redux (Versão Simples)</h1>
-      <div className="linha">
-      <Card/>  
-      <Card/>  
-      <Card/>  
-      </div>
-      
 
-      <div className="linha">
-      <Card/>  
-      <Card/>  
-      <Card/>  
+      <div className="linha"> 
+        <Card title="Card 1" red >X</Card>  
+      
+        <Card title="Card 2" green>Y</Card> 
+      
       </div>
+      <div className="linha"> 
+      
+        <Card title="Card 3" blue>Y</Card> 
+      
+        <Card title="Card 4" purple>Y</Card> 
     
-    
+    </div>
     </div>
   );
 }
