155689d30a42fcba7282d8a6bb23aec62d03b54a
viannaandreBR <vianna_andre@hotmail.com@users.noreply.github.com>
2020-07-13 22:59:11 -0300

    Redux Config with  <Provider> integration React-Redux

--- redux-simples-com-redux/src/components/Intervalo.jsx
+++ redux-simples-com-redux/src/components/Intervalo.jsx
@@ -4,8 +4,7 @@ import React from 'react'
 import Card from './Card'
 
 export default props => {
-  const min = props.min
-  const max = props.max
+
 
   return (
     <Card title="Intervalo de Números" red>
